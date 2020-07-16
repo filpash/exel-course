ad3f62c6f1803b6058fe9dfa37e285050fb1408d
Filipp <fili92@mail.ru>
2020-07-16 23:38:00 +0300

    create table cell

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,14 +3,9 @@ const CODES = {
   Z: 90
 }
 
-const COUNT = {
-    count1: 1,
-    count15: 15
-}
-
-function createCell() {
+function toCell() {
   return `
-        <div class="cell" contenteditable>B2</div>
+        <div class="cell" contenteditable></div>
     `
 }
 
@@ -20,10 +15,10 @@ function toColumn(col) {
     `
 }
 
-function createRow(content) {
+function createRow(index, content) {
   return `
         <div class="row">
-            <div class="row-info"></div>
+            <div class="row-info">${index ? index : ''}</div>
             <div class="row-data">${content}</div>
         </div>
     `
@@ -35,7 +30,6 @@ function toChar(_, index) {
 
 export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1
-  const cellCount = COUNT.count15 - COUNT.count1 + 1
   const rows = []
   const cols = new Array(colsCount)
       .fill('')
@@ -43,14 +37,15 @@ export function createTable(rowsCount = 15) {
       .map(toColumn)
       .join('')
 
-  rows.push(createRow(cols))
+  rows.push(createRow(null, cols))
 
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow())
-  }
+    const cell = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('')
 
-  for (let i = 0; i < cellCount; i++) {
-    rows.push(cellCount())
+    rows.push(createRow(i + 1, cell))
   }
 
   return rows.join('')
