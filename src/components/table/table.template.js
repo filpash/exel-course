b24529706e9c8a48f36820b51ee904bf35286f96
Filipp Pashkevich <48940744+filpash@users.noreply.github.com>
2020-07-21 00:40:00 +0300

    Resize (#6)
    * set elements for resize col & row
    
    * changing col-size table
    
    * changing size rows & cols & their optim-n
    
    * util for work with styles
    
    * unreal optimization for resize
    
    * Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,25 +3,25 @@ const CODES = {
   Z: 90
 }
 
-function toCell() {
+function toCell(_, col) {
   return `
-        <div class="cell" contenteditable></div>
+        <div class="cell" contenteditable data-col="${col}"></div>
     `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-        <div class="column">
+        <div class="column" data-type="resizable" data-col="${index}">
             ${col}
-            <div class="col-resize"></div>
+            <div class="col-resize" data-resize="col"></div>
         </div>
     `
 }
 
 function createRow(index, content) {
-  const resize = index ? '<div class="row-resize"></div>' : ''
+  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
   return `
-        <div class="row">
+        <div class="row" data-type="resizable">
             <div class="row-info">
                 ${index ? index : ''}
                 ${resize}
@@ -32,7 +32,7 @@ function createRow(index, content) {
 }
 
 function toChar(_, index) {
-    return String.fromCharCode(CODES.A + index)
+  return String.fromCharCode(CODES.A + index)
 }
 
 export function createTable(rowsCount = 15) {
@@ -57,3 +57,5 @@ export function createTable(rowsCount = 15) {
 
   return rows.join('')
 }
+
+
